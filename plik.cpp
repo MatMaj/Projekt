9ae33b6454eb5c327f1b0a1c69538becf8a53e34
mTemus <email>
2018-01-20 21:54:30 +0100

    funkcja1

---
+++ plik.cpp
@@ -0,0 +1,22 @@
+
+    int tablica[10];
+    for (int i = 0; i < 10; i++)
+    {
+        tablica[i] = rand();
+        cout << tablica[i] << endl;
+
+    }
+
+    system(\"pause\");
+    return 0;
+   
+    //commit 2
+   
+        int tablica[10];
+    int dwa[10];
+    for (int i = 0; i < 10; i++)
+    {
+        tablica[i] = rand();
+        cout << tablica[i] << endl;
+
+    }
