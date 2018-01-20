f7411690986579f3aa404801639486457572bfb2
MatMaj <matmaj81@wp.pl>
2018-01-20 20:52:50 +0100

    plik glowny

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <iostream>
+#include "mateusz.cpp"
+using namespace std;
+
+int main(){
+
+setlocale(LC_ALL,"");
+
+int wybor=0;
+cout<<"=======MENU======="<<endl;
+cout<<"1.Wybor 1"<<endl;
+cout<<"2.Wybor 2"<<endl;
+cout<<"3.Wybor 3"<<endl;
+cout<<"4.Wybor 4"<<endl<<endl;
+cout << "Wybierz pozycję z menu: ";
+cin >> wybor;
+
+switch(wybor){
+case 1:
+	break;
+case 2:
+	break;
+case 3:
+	break;
+case 4:
+	break;
+}
+system("pause");
+return 0;
+}
