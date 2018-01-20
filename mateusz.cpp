815531b28bab30f7ddd5fa34867737136e94aa71
MatMaj <matmaj81@wp.pl>
2018-01-20 21:32:57 +0100

    mateusz funkcjav4

--- mateusz.cpp
+++ mateusz.cpp
@@ -2,13 +2,16 @@ void mateuszf(){
 //Losowanie randomowych liczb - rzut kostką 6 ścienną za pomocą funkcji rand c++
 srand( time( NULL ) );
 //zmienne
-int suma,ilrz;
+int suma=0;
+int ilrz=0;
+int rzut=0;
 //Podaj liczbę rzutów kostką
 cout << "Podaj liczę rzutów kostką: ";
 cin << ilrz;
 //Pętla losowania 
 for (int i=0;i<=ilrz;i++){
-
+rzut=rand()%6+1;
+cout<<i<<" losowanie: "<<rzut; 
 }
 }
 
